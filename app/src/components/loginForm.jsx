d433eed8387afb69dcceb54b789ef6c439710ad9
Jean Loureiro <jean_loureiro@hotmail.com>
2019-04-03 20:42:53 +1000

    [add] Register and login endpoint

--- app/src/components/loginForm.jsx
+++ app/src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Joi from 'joi-browser'
 import Form from './common/form'
+import { login } from '../services/authService'
 
 class LoginForm extends Form {
 
@@ -21,10 +22,18 @@ class LoginForm extends Form {
     }
 
 
-    doSubmit = () => {
-        // call the server
-
-        console.log('Submitted!!!')
+    doSubmit = async () => {
+        try{
+            const { data } = this.state
+            await login( data.username, data.password )
+        }
+        catch(ex){
+            if( ex.response && ex.response.status === 400 ){
+                const errors = { ...this.state.errors }
+                errors.username = ex.response.data
+                this.setState({ errors })
+            }
+        }
     }
 
     render() {
--- app/src/components/registerForm.jsx
+++ app/src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import Joi from 'joi-browser'
 import Form from './common/form'
+import * as userService from '../services/userService'
 
 class registerForm extends Form {
 
@@ -24,10 +25,17 @@ class registerForm extends Form {
     }
 
 
-    doSubmit = () => {
-        // call the server
-
-        console.log('Submitted!!!')
+    doSubmit = async () => {
+        try{
+            await userService.register(this.state.data)
+        }
+        catch(ex){
+            if(ex.response && ex.response.status === 400 ){
+                const errors = { ...this.state.errors }
+                errors.username = ex.response.data
+                this.setState({ errors })
+            }
+        }
     }
 
     render() {
